14346929b1b3b87b47520c0a3d952ac061736601
PaulEgbe <paulegbe74@gmail.com>
2025-04-23 18:43:46 -0500

    Initial Commit

---
+++ DailyAffirmations/DailyAffirmationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyAffirmationsApp.swift
+//  DailyAffirmations
+//
+//  Created by Paul Egbe on 4/23/25.
+//
+
+import SwiftUI
+
+@main
+struct DailyAffirmationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
